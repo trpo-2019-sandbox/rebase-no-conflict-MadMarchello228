73be81cd6efad0eecff999cee3da9583ff41a29e
U-MADMARCHELLO228\Марк <mark1010_00@mail.ru>
2019-03-31 21:44:32 +0700

    Fixed error in main.c

--- main.c
+++ main.c
@@ -2,38 +2,35 @@
 
 #define N_ELEMENTS(arr) (sizeof(arr) / sizeof(*(arr)))
 
-int min_element(int* arr, int size)
-{
-    int min = *arr;
+int min_element(int* arr, int size) {
+  int min = *arr;
 
-    int* it;
-    for (it = arr + 1; it < arr + size; ++it) {
-        if (*it < min) {
-            min = *it;
-        }
+  int* it;
+  for (it = arr + 1; it < arr + size; ++it) {
+    if (*it < min) {
+      min = *it;
     }
+  }
 
-    return min;
+  return min;
 }
 
-void print_array(int* arr, int size)
-{
-    printf("Array: ");
-    int i;
-    for (i = 0; i < N_ELEMENTS(arr); ++i) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+void print_array(int* arr, int size) {
+  printf("Array: ");
+  int i;
+  for (i = 0; i < size; ++i) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
 }
 
-int main()
-{
-    int arr[] = {3, 1, 4, 1, 5, 9, 2};
+int main() {
+  int arr[] = {3, 1, 4, 1, 5, 9, 2};
 
-    const int min = min_element(arr, N_ELEMENTS(arr));
+  const int min = min_element(arr, N_ELEMENTS(arr));
 
-    print_array(arr, N_ELEMENTS(arr));
-    printf("Min element: %d\n", min);
+  print_array(arr, N_ELEMENTS(arr));
+  printf("Min element: %d\n", min);
 
-    return 0;
+  return 0;
 }
